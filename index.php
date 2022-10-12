24736c948603209ea6c927e8db57a1d34500a1af
김정욱 <95655442+rlawjddnr0523@users.noreply.github.com>
2022-10-12 19:00:09 +0900

    ghgh

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<header>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./stylesheet.css">
+    <script src="https://kit.fontawesome.com/d2e652c883.js" crossorigin="anonymous"></script>
+</header>
+<body>
+    <div class="index-1 header top">
+        <div class="title-img img1"><img src="./eddmaster.png" width="300" height="27"></div>
+    </div>
+    <div class="index-2 header top">
+        <form method="get" action="search.php">
+            <div class="textbox">
+                <label for="lb">검색어를 입력하세요.</label>
+                <input type="text" id="lb" class="textbox-1">
+            </div>
+            <button type="submit" class="smb-1"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+    </div>
+    <div class="index-3 header">
+        <div class="topnav nav">
+            <div class="nav-1">
+                갤러리
+            </div>
+            <div class="nav-2">
+                마이너 갤러리
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+
+</footer>
+</html>
